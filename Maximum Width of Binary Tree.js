cfbe07270f1332db7eaa2881da7f0d47d71a2cd7
Anandu-Vijayan <ananduvijyan8@gmail.com>
2026-01-19 22:02:14 +0530

    Maximum Width of Binary Tree

---
+++ Maximum Width of Binary Tree.js
@@ -0,0 +1,67 @@
+// 662. Maximum Width of Binary Tree
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Given the root of a binary tree, return the maximum width of the given tree.
+
+// The maximum width of a tree is the maximum width among all levels.
+
+// The width of one level is defined as the length between the end-nodes (the leftmost and rightmost non-null nodes), where the null nodes between the end-nodes that would be present in a complete binary tree extending down to that level are also counted into the length calculation.
+
+// It is guaranteed that the answer will in the range of a 32-bit signed integer.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,3,2,5,3,null,9]
+// Output: 4
+// Explanation: The maximum width exists in the third level with length 4 (5,3,null,9).
+// Example 2:
+
+
+// Input: root = [1,3,2,5,null,null,9,6,null,7]
+// Output: 7
+// Explanation: The maximum width exists in the fourth level with length 7 (6,null,null,null,null,null,7).
+// Example 3:
+
+
+// Input: root = [1,3,2,5]
+// Output: 2
+// Explanation: The maximum width exists in the second level with length 2 (3,2).
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 3000].
+// -100 <= Node.val <= 100
+
+
+var widthOfBinaryTree = function(root) {
+    if (!root) return 0;
+    let maxWidth = 0;
+    const queue = [[root, 0]];
+
+    while (queue.length > 0) {
+        const size = queue.length;
+        const levelStart = queue[0][1];
+        let first = 0, last = 0;
+
+        for (let i = 0; i < size; i++) {
+            const [node, index] = queue.shift();
+            const normalizedIndex = index - levelStart;
+            if (i === 0) first = normalizedIndex;
+            if (i === size - 1) last = normalizedIndex;
+
+            if (node.left) queue.push([node.left, 2 * normalizedIndex]);
+            if (node.right) queue.push([node.right, 2 * normalizedIndex + 1]);
+        }
+
+        maxWidth = Math.max(maxWidth, last - first + 1);
+    }
+
+    return maxWidth;
+};
